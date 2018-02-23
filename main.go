7ee174df8fab91060479764f2f364f5d34daa719
unknown <yblues@hotmail.com>
2018-02-23 17:15:54 +0800

    f

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 var (
-	dbhost       = "rm-bp18iy73784671903yo.mysql.rds.aliyuncs.com"
+	dbhost       = ""
 	dbport       = 3306
-	dbname       = "dutyfree"
-	dbuser       = "root_xw"
-	dbpass       = "Xw_19920602_wX"
+	dbname       = ""
+	dbuser       = ""
+	dbpass       = ""
 	charset      = "utf8mb4"
 	maxIdleConns = 1000
 	maxOpenConns = 1000
